9cfd87768062368f8205e2a443529caf4f75eb6b
Gabriel Acosta <acostadariogabriel@gmail.com>
2014-07-05 16:35:30 -0300

    Agregado: acción para reportar bugs (menu-acerca_de)

--- edis_c/interfaz/menu/menu_acerca_de.py
+++ edis_c/interfaz/menu/menu_acerca_de.py
@@ -4,9 +4,12 @@
 menu acerca de
 
 """
+import webbrowser
+
 from PyQt4.QtGui import QMessageBox
 from PyQt4.QtCore import QObject
 
+import edis_c
 from edis_c.interfaz.dialogos import dialogo_acerca_de_ide
 
 
@@ -17,12 +20,18 @@ class MenuAcercade(QObject):
 
         # Contenedor del Widget Principal
         self.ide = ide
+        reportarBugs = menu_acerca_de.addAction(self.trUtf8("Reportar bugs!"))
+        menu_acerca_de.addSeparator()
         acerca_ide = menu_acerca_de.addAction(self.tr("Acerca de IDE"))
         acerca_qt = menu_acerca_de.addAction(self.tr("Acerca de Qt"))
 
         # Conexiones
+        reportarBugs.triggered.connect(self.reportar_bugs_)
         acerca_ide.triggered.connect(self.acerca_de_ide)
         acerca_qt.triggered.connect(self.acerca_de_qt)
+
+    def reportar_bugs_(self):
+        webbrowser.open(edis_c.__reportar_bug__)
 
     def acerca_de_ide(self):
         self.acerca_de = dialogo_acerca_de_ide.AcercaDeIDE()
