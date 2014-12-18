f89413c3fb28265b9b70eb0f0f0cdebebce98b4b
Gabo <acostadariogabriel@gmail.com>
2014-12-18 14:21:21 -0300

    Fixed minimapa (implementado con QPlainTextEdit)

--- src/ui/editor/minimapa.py
+++ src/ui/editor/minimapa.py
@@ -5,170 +5,148 @@
 # Copyright 2014 - Gabriel Acosta
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 
-""" Minimapa """
+#TODO: Cambiar QPlainTextEdit por QsciScintilla
+#FIXME: Resaltado de sintaxis
 
-# Módulos QtGui
-from PyQt4.QtGui import QPlainTextEdit
-from PyQt4.QtGui import QTextOption
-from PyQt4.QtGui import QFrame
-from PyQt4.QtGui import QFontMetrics
-from PyQt4.QtGui import QGraphicsOpacityEffect
+from PyQt4.QtGui import (
+    QPlainTextEdit,
+    QFrame,
+    QFontMetrics,
+    QGraphicsOpacityEffect
+    )
 
-# Módulos QtCore
-from PyQt4.QtCore import SIGNAL
-from PyQt4.QtCore import Qt
-from PyQt4.QtCore import QPropertyAnimation
-
-# Módulos EDIS
-from src import recursos
-from src.helpers import configuraciones
+from PyQt4.QtCore import (
+    QPropertyAnimation,
+    Qt
+    )
 
 
 class MiniMapa(QPlainTextEdit):
 
-    def __init__(self, parent):
-        super(MiniMapa, self).__init__(parent)
+    lineas = 0
 
-        self.setWordWrapMode(QTextOption.NoWrap)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+    def __init__(self, editor):
+        super(MiniMapa, self).__init__(editor)
+        # Configuración QPlainTextEdit
         self.setReadOnly(True)
-        self.setCenterOnScroll(True)
         self.setMouseTracking(True)
-        self.viewport().setCursor(Qt.PointingHandCursor)
-        self.setTextInteractionFlags(Qt.NoTextInteraction)
-        self.setStyleSheet("background: transparent;")
 
-        self.parent = parent
-        self.lineas = 0
-        self.highlighter = None
+        self.editor = editor
+        self.setStyleSheet("background: transparent; color: white")
+        # Deslizador
+        self.deslizador = Deslizador(self)
+        self.deslizador.show()
 
-        self.connect(self.parent, SIGNAL("updateRequest(const QRect&, int)"),
-            self.actualizar_area_visible)
-
+        # Efecto y animación
         self.efecto = QGraphicsOpacityEffect()
         self.setGraphicsEffect(self.efecto)
         self.efecto.setOpacity(0.07)
         self.animacion = QPropertyAnimation(self.efecto, "opacity")
 
-        self.slider = Slider(self)
-        self.slider.show()
-
-    def calcular_max(self):
-        altura_de_linea = self.parent.cursorRect().height()
-        if altura_de_linea > 0:
-            self.lineas = self.parent.viewport().height() / altura_de_linea
-        self.slider.actualizar_posicion()
-        self.actualizar_area_visible()
-
-    def set_code(self, codigo):
+    def codigo(self, codigo):
         self.setPlainText(codigo)
-        self.calcular_max()
+        self.altura_lineas()
+        self.area()
 
-    def actualizar_area_visible(self):
-        if not self.slider.pressed:
-            numero_linea = self.parent.firstVisibleBlock().blockNumber()
-            bloque = self.document().findBlockByLineNumber(numero_linea)
+    def area(self):
+        if not self.deslizador.presionado:
+            linea, c = self.editor.devolver_posicion_del_cursor()
+            bloque = self.document().findBlockByNumber(linea)
             cursor = self.textCursor()
             cursor.setPosition(bloque.position())
-            rect = self.cursorRect(cursor)
+            rec = self.cursorRect(cursor)
             self.setTextCursor(cursor)
-            self.slider.mover_slider(rect.y())
+            self.deslizador.mover(rec.y())
 
-    def enterEvent(self, event):
-        self.animacion.setDuration(400)
-        self.animacion.setStartValue(configuraciones.OPAC_MIN)
-        self.animacion.setEndValue(configuraciones.OPAC_MAX)
-        self.animacion.start()
-
-    def leaveEvent(self, event):
-        self.animacion.setDuration(400)
-        self.animacion.setStartValue(configuraciones.OPAC_MAX)
-        self.animacion.setEndValue(configuraciones.OPAC_MIN)
-        self.animacion.start()
-
-    def mousePressEvent(self, event):
-        super(MiniMapa, self).mousePressEvent(event)
-        cursor = self.cursorForPosition(event.pos())
-        self.parent.saltar_a_linea(cursor.blockNumber())
-
-    def resizeEvent(self, event):
-        super(MiniMapa, self).resizeEvent(event)
-        self.slider.actualizar_posicion()
-
-    def scroll_area(self, pos_p, pos_slider):
-        pos_p.setY(pos_p.y() - pos_slider.y())
-        cursor = self.cursorForPosition(pos_p)
-        self.parent.verticalScrollBar().setValue(cursor.blockNumber())
-
-    def wheelEvent(self, event):
-        super(MiniMapa, self).wheelEvent(event)
-        self.parent.wheelEvent(event)
-
-    def ajustar_(self):
-        self.setFixedHeight(self.parent.height())
-        self.setFixedWidth(self.parent.width() * configuraciones.MINI_TAM)
-        x = self.parent.width() - self.width()
-        self.move(x, 0)
-        tam_fuente = int(self.width() / configuraciones.MARGEN)
-        if tam_fuente < 1:
-            tam_fuente = 1
+    def redimensionar(self):
+        ancho = self.editor.width() * 0.17
+        altura = self.editor.height()
+        self.setFixedSize(ancho, altura)
+        self.mover(self.editor.width() - self.width(), 0)
+        #FIXME: Márgen de línea
+        tam_fuente = self.width() / 80
         fuente = self.document().defaultFont()
         fuente.setPointSize(tam_fuente)
         self.setFont(fuente)
-        self.calcular_max()
+        self.altura_lineas()
+
+    def altura_lineas(self):
+        altura = self.editor.altura_lineas
+        if altura > 0:
+            self.lineas = self.editor.viewport().height() / altura
+        self.deslizador.ajustar()
+
+    def mover(self, x, y):
+        self.move(x, y)
+
+    def scroll_area(self, pos, pos_des):
+        pos.setY(pos.y() - pos_des.y())
+        cursor = self.cursorForPosition(pos)
+        self.editor.verticalScrollBar().setValue(cursor.blockNumber())
+
+    def resizeEvent(self, e):
+        super(MiniMapa, self).resizeEvent(e)
+        self.deslizador.ajustar()
+
+    def enterEvent(self, e):
+        self.animacion.setDuration(400)
+        self.animacion.setStartValue(0.07)
+        self.animacion.setEndValue(0.30)
+        self.animacion.start()
+
+    def leaveEvent(self, e):
+        self.animacion.setDuration(400)
+        self.animacion.setStartValue(0.30)
+        self.animacion.setEndValue(0.07)
+        self.animacion.start()
 
 
-class Slider(QFrame):
+class Deslizador(QFrame):
 
-    def __init__(self, parent):
-        super(Slider, self).__init__(parent)
-        self.parent = parent
-        self.setMouseTracking(True)
-        self.setCursor(Qt.OpenHandCursor)
-
-        color = recursos.TEMA_EDITOR['num-seleccionado']
-        self.setStyleSheet("background: %s;" % color)
+    def __init__(self, minimapa):
+        super(Deslizador, self).__init__(minimapa)
+        self.minimapa = minimapa
         self.efecto = QGraphicsOpacityEffect()
         self.setGraphicsEffect(self.efecto)
-        self.efecto.setOpacity(0.2)
-        self.pressed = False
+        self.efecto.setOpacity(0.4)
+        self.setStyleSheet("background: blue")
+        self.setMouseTracking(True)
+        self.setCursor(Qt.OpenHandCursor)
+        self.presionado = False
         self.scroll_margen = None
 
-    def actualizar_posicion(self):
-        tam_fuente = QFontMetrics(self.parent.font()).height()
-        altura = self.parent.lineas * tam_fuente
+    def ajustar(self):
+        fuente = QFontMetrics(self.minimapa.font()).height()
+        altura = self.minimapa.lineas * fuente
         self.setFixedHeight(altura)
-        self.setFixedWidth(self.parent.width())
-        self.scroll_margen = (altura, self.parent.height() - altura)
+        self.setFixedWidth(self.minimapa.width())
+        self.scroll_margen = (altura, self.minimapa.height() - altura)
 
-    def mover_slider(self, y):
+    def mover(self, y):
         self.move(0, y)
 
-    def mousePressEvent(self, event):
-        super(Slider, self).mousePressEvent(event)
-        self.pressed = True
+    def mousePressEvent(self, e):
+        super(Deslizador, self).mousePressEvent(e)
+        self.presionado = True
         self.setCursor(Qt.ClosedHandCursor)
 
-    def mouseReleaseEvent(self, event):
-        super(Slider, self).mouseReleaseEvent(event)
-        self.pressed = False
+    def mouseReleaseEvent(self, e):
+        super(Deslizador, self).mouseReleaseEvent(e)
+        self.presionado = False
         self.setCursor(Qt.OpenHandCursor)
 
-    def mouseMoveEvent(self, event):
-        super(Slider, self).mouseMoveEvent(event)
-        if self.pressed:
-            pos = self.mapToParent(event.pos())
-            y = pos.y() - (self.height() / 2)
+    def mouseMoveEvent(self, e):
+        super(Deslizador, self).mouseMoveEvent(e)
+        if self.presionado:
+            posicion = self.mapToParent(e.pos())
+            y = posicion.y() - self.height() / 2
             if y < 0:
                 y = 0
             if y < self.scroll_margen[0]:
-                self.parent.verticalScrollBar().setSliderPosition(
-                    self.parent.verticalScrollBar().sliderPosition() - 2)
-
-            elif y > self.scroll_margen[1]:
-                self.parent.verticalScrollBar().setSliderPosition(
-                    self.parent.verticalScrollBar().sliderPosition() + 2)
-
+                self.minimapa.verticalScrollBar().setSliderPosition(
+                    self.minimapa.verticalScrollBar().sliderPosition() - 2)
+            elif y < self.scroll_margen[1]:
+                self.minimapa.verticalScrollBar().setSliderPosition(
+                    self.minimapa.verticalScrollBar().sliderPosition() + 2)
             self.move(0, y)
-            self.parent.scroll_area(pos, event.pos())
+            self.minimapa.scroll_area(posicion, e.pos())
