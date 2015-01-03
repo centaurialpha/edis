8318d763754f990106b930cf65b115b77d649fca
Gabo <acostadariogabriel@gmail.com>
2015-01-02 23:31:22 -0300

    Actualizando año

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -2,7 +2,7 @@
 # EDIS - Entorno de Desarrollo Integrado Simple para C/C++
 #
 # This file is part of EDIS
-# Copyright 2014 - Gabriel Acosta
+# Copyright 2014-2015 - Gabriel Acosta
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 
 __descripcion__ = """
