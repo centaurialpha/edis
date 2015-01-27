7efe8d63bc401db318789404f2078fb8489e0409
Gabo <acostadariogabriel@gmail.com>
2015-01-27 05:16:28 -0300

    Fixed about dialog

--- src/ui/dialogos/acerca_de.py
+++ src/ui/dialogos/acerca_de.py
@@ -20,20 +20,29 @@ from PyQt4.QtGui import (
     QLabel
     )
 
+from PyQt4.QtCore import Qt
+
 from src import recursos
 from src import ui
 
 
 class AcercaDe(QDialog):
 
-    def __init__(self, parent=None):
-        super(AcercaDe, self).__init__(parent)
+    def __init__(self, parent):
+        QDialog.__init__(self, parent, Qt.WindowMinMaxButtonsHint)
         self.setWindowTitle(self.tr("Acerca de EDIS"))
-        self.setMinimumWidth(400)
         box = QVBoxLayout(self)
+        label_logo = QLabel()
+        label_logo.setPixmap(QPixmap(recursos.ICONOS['icon']))
+        label_titulo = QLabel(self.tr("<h1>EDIS</h1>\n<i>Simple Integrated "
+                            "Development Environment</i>"))
+        box_logo = QHBoxLayout()
         self.tabs = QTabWidget()
         self.tabs.addTab(AboutTab(), self.tr("Acerca de"))
         self.tabs.addTab(ReportarBugTab(), self.tr("Reportar bug"))
+        box_logo.addWidget(label_logo)
+        box_logo.addWidget(label_titulo)
+        box.addLayout(box_logo)
         box.addWidget(self.tabs)
 
         box_boton = QHBoxLayout()
@@ -52,16 +61,25 @@ class AboutTab(QWidget):
         box = QVBoxLayout(self)
         box.setContentsMargins(0, 0, 0, 0)
         lbl_version = QLabel(self.tr("Versión: %s") % ui.__version__)
-        lbl_link = QLabel("<a href='%s'><span style='color: #dedede;'>%s</span>"
-                          "</a>" % (ui.__web__, ui.__web__))
-        logo = QPixmap(recursos.ICONOS['about'])
+        label_descripcion = QLabel(self.tr("a simple cross-platform IDE for C"))
+        lbl_link = QLabel("Web: <a href='%s'><span style='color: #dedede;'>"
+                        "%s</span></a>" % (ui.__web__, ui.__web__))
+        lbl_sc = QLabel(self.tr("Código fuente: <a href='%s'><span style="
+                        "'color: #dedede;'>%s</span></a>" %
+                        (ui.__codigo_fuente__, ui.__codigo_fuente__)))
+        logo = QPixmap(recursos.ICONOS['icon'])
         label_logo = QLabel()
+        label_logo.setAlignment(Qt.AlignCenter)
         label_logo.setPixmap(logo)
-        box.addWidget(label_logo)
+        box.addWidget(label_descripcion)
         box.addWidget(lbl_version)
         box.addWidget(lbl_link)
+        box.addWidget(lbl_sc)
+        box.addItem(QSpacerItem(1, 0, QSizePolicy.Expanding,
+                    QSizePolicy.Expanding))
 
         lbl_link.linkActivated.connect(lambda link: webbrowser.open(link))
+        lbl_sc.linkActivated.connect(lambda link: webbrowser.open(link))
 
 
 class ReportarBugTab(QWidget):
