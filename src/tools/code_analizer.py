bb35685335163c62ee2fdfe415564b14486061d9
Gabo <acostadariogabriel@gmail.com>
2015-02-13 21:58:40 -0300

    Se agrega el módulo pycparser para no depender de Ctags

--- src/tools/code_analizer.py
+++ src/tools/code_analizer.py
@@ -6,7 +6,8 @@
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 
 import re
-from pycparser import (
+
+from .pycparser import (
     c_parser,
     c_ast
     )
@@ -15,10 +16,11 @@ from pycparser import (
 def parse_symbols(source):
     """ Parsea el código fuente para obtener los símbolos:
 
-        Estructuras, variables, funciones.
+        Estructuras, uniones, variables, funciones.
     """
 
     functions = {}
+
     parser = c_parser.CParser()
     # AST Abstract Syntax Tree
     ast = parser.parse(source)
@@ -28,12 +30,31 @@ def parse_symbols(source):
             function_name = ast_object.decl.name
             function_nline = ast_object.decl.coord.line
             functions[function_nline] = function_name
+        elif ast_object.__class__ is c_ast.Decl:
+            if ast_object.type.__class__ is c_ast.Struct:
+                parse_structs(ast_object)
 
     return {'functions': functions}
 
 
+def parse_structs(ast_object):
+    structs = {}
+    _members = []
+
+    members = ast_object.type.decls
+    struct_name = ast_object.type.name
+    struct_nline = ast_object.type.coord.line
+
+    for member in members:
+        member_name = member.name
+        member_nline = member.coord.line
+        _members.append({member_nline: member_name})
+
+    print(_members)
+
+
 def sanitize_source_code(source_code):
-    """ Ignora comentarios y declarativas """
+    """ Ignora comentarios y declarativas del preprocesador """
 
     def blot_out_non_new_lines(str_in):
         return "" + ("\n" * str_in.count('\n'))
