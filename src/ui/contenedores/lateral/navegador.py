7bf4752f94aa97fdf572c49fb7255ee91b71a3a3
Gabo <acostadariogabriel@gmail.com>
2015-02-21 14:30:27 -0300

    Change copyright

--- src/ui/contenedores/lateral/navegador.py
+++ src/ui/contenedores/lateral/navegador.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # EDIS - a simple cross-platform IDE for C
 #
-# This file is part of EDIS
-# Copyright 2014-2015 - Gabriel Acosta
+# This file is part of Edis
+# Copyright 2014-2015 - Edis Team
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 
 from PyQt4.QtGui import QListWidget
--- tests/test_preferencias.py
+++ tests/test_preferencias.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # EDIS - Entorno de Desarrollo Integrado Simple para C/C++
 #
-# This file is part of EDIS
-# Copyright 2014-2015 - Gabriel Acosta
+# This file is part of Edis
+# Copyright 2014-2015 - Edis Team
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 
 import os
