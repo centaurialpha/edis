6ca52c588a2482b6bc39c23a6ded4e16a4ac90a5
Gabo <acostadariogabriel@gmail.com>
2015-03-04 07:10:22 -0300

    Code analizer tests

---
+++ tests/test_code_analizer.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# EDIS - a simple cross-platform IDE for C
+#
+# This file is part of Edis
+# Copyright 2014-2015 - Edis Team
+# License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
+
+import unittest
+
+from src.tools import code_analizer
+
+CODE = """
+#include <stdio.h>
+FILE * fp;
+struct ufo {
+    int a;
+};
+//testing
+void main( void ) {
+    return;
+}
+"""
+
+CODE_BUG = "#include <stdio.h>\nFILE * fp;\nvoid main( void ){"
+
+
+class CodeAnalizerTestCase(unittest.TestCase):
+
+    def test_parse_symbols(self):
+        self.assertEqual(({}, {}), code_analizer.parse_symbols(CODE_BUG))
+
+    def test_sanitize_source_coude(self):
+        expected = "\n\n\nstruct ufo {\n    int a;\n};\n\nvoid main( void ) " \
+                   "{\n    return;\n}\n"
+        self.assertEqual(expected, code_analizer.sanitize_source_code(CODE))
+
+if __name__ == "__main__":
+    unittest.main()
