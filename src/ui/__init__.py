fa4ef6d8022df2d9a777a71eb0936fabd339f396
Gabo <acostadariogabriel@gmail.com>
2015-03-06 18:30:04 -0300

    Change version

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -7,7 +7,7 @@
 
 __descripcion__ = """
 <b>EDIS</b> es un Entorno de Desarrollo Integrado (IDE) libre y multiplataforma
-para los lenguajes de programación C/C++ desarrollado completamente en Python.
+para el lenguaje de programación C desarrollado completamente en Python.
 <br>
 """
 __nombre__ = "EDIS"
@@ -15,12 +15,10 @@ __autor__ = "Gabriel Acosta"
 __email_autor__ = "acostadariogabriel@gmail.com"
 __codigo_fuente__ = "http://github.com/centaurialpha/edis"
 __web__ = "http://centaurialpha.github.io/edis"
-__version__ = "1.0-beta"
+__version__ = "1.0"
 __python__ = "https://www.python.org/"
 __qt__ = "https://qt.digia.com/"
 __pyqt__ = "http://www.riverbankcomputing.co.uk/software/pyqt/intro"
 __reportar_bug__ = "http://github.com/centaurialpha/edis/issues/new"
 __version_web__ = \
     "https://raw.githubusercontent.com/centaurialpha/edis/master/version"
-__gcc__ = \
-    "http://sourceforge.net/projects/mingw/files/Installer/mingw-get-setup.exe"
