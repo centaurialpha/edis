c04eacdc28f690c63a573361030b6a6dc49f6d31
Gabo <acostadariogabriel@gmail.com>
2015-03-06 19:49:25 -0300

    Delete build

--- build/lib/src/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# EDIS - a simple cross-platform IDE for C
-#
-# This file is part of Edis
-# Copyright 2014-2015 - Edis Team
-# License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
-
-from src import resources  # lint:ok
