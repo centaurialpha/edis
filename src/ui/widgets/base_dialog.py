2f0dffa4ce1c21c35bcf7a5ac40cd3a8b921aee8
Gabo <acostadariogabriel@gmail.com>
2015-03-11 09:33:36 -0300

    Fix: QPropertyAnimation on base dialog

--- src/ui/widgets/base_dialog.py
+++ src/ui/widgets/base_dialog.py
@@ -5,11 +5,18 @@
 # Copyright 2014-2015 - Edis Team
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 
-from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import (
+    QDialog,
+    QShortcut,
+    QKeySequence
+    )
 
 from PyQt4.QtCore import (
     Qt,
-    #QPoint
+    QPropertyAnimation,
+    QRect,
+    QEasingCurve,
+    SIGNAL
     )
 
 
@@ -26,7 +33,25 @@ class BaseDialog(QDialog):
         # Posición
         self.qpoint = weditor.rect().topRight()
         self.global_point = weditor.mapToGlobal(self.qpoint)
+        # Animación
+        self._animation = QPropertyAnimation(self, "geometry")
+        self._animation.setDuration(150)
+        self._animation.setEasingCurve(QEasingCurve.Linear)
+
+        key_escape = QShortcut(QKeySequence(Qt.Key_Escape), self)
+        self.connect(key_escape, SIGNAL("activated()"), self._close)
 
     def showEvent(self, event):
         super(BaseDialog, self).showEvent(event)
+        x, y = self.geometry().x(), self.geometry().y()
+        self._animation.setStartValue(QRect(x, 0, self.width(), self.height()))
+        self._animation.setEndValue(QRect(x, y, self.width(), self.height()))
+        self._animation.start()
         self.line.setFocus()
+
+    def _close(self):
+        x, y = self.geometry().x(), self.geometry().y()
+        self._animation.setStartValue(QRect(x, y, self.width(), self.height()))
+        self._animation.setEndValue(QRect(x, 0, self.width(), self.height()))
+        self._animation.start()
+        self.connect(self._animation, SIGNAL("finished()"), self.close)
