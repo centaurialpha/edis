3fb2b45faa51fca98b5f1282c9675525d79a33fc
Gabo <acostadariogabriel@gmail.com>
2015-03-19 23:28:05 -0300

    Fixed style checker, matched warning in comments

--- src/ui/dialogs/about.py
+++ src/ui/dialogs/about.py
@@ -34,28 +34,29 @@ class AcercaDe(QDialog):
         self.setWindowTitle(self.tr("About Edis"))
         self.setMinimumWidth(485)
         box = QVBoxLayout(self)
-        label_logo = QLabel()
-        label_logo.setPixmap(QPixmap(":image/edis"))
-        label_titulo = QLabel(self.tr("<h1>Edis</h1>\n<i>a simple "
-                              "cross-platform IDE for C</i>"))
+        #label_logo = QLabel()
+        #label_logo.setPixmap(QPixmap(":image/edis"))
+        title_label = QLabel(self.tr("<h1>Edis</h1>\n<i>a simple "
+                                     "cross-platform IDE for C</i>"))
+        title_label.setAlignment(Qt.AlignRight)
         box_logo = QHBoxLayout()
-        box_logo.addWidget(label_logo)
-        box_logo.addWidget(label_titulo)
+        #box_logo.addWidget(label_logo)
+        box_logo.addWidget(title_label)
         box.addLayout(box_logo)
         lbl_version = QLabel(self.tr("Version: {0}").format(ui.__version__))
         box.addWidget(lbl_version)
-        lbl_link = QLabel("Web: <a href='%s'><span style='color: #0197FD;'>"
-                          "%s</span></a>" % (ui.__web__, ui.__web__))
-        lbl_sc = QLabel(self.tr("Source code: <a href='{0}'><span style="
-                        "'color: #0197FD;'>{1}</span></a>").format(
-                        ui.__codigo_fuente__, ui.__codigo_fuente__))
+        lbl_link = QLabel("<a href='%s'><span style='color: #0197FD;'>"
+                          "Web</span></a>" % ui.__web__)
+        lbl_sc = QLabel(self.tr("<a href='{0}'><span style="
+                        "'color: #0197FD;'>Source Code</span></a>").format(
+                        ui.__codigo_fuente__))
         box.addWidget(lbl_link)
         box.addWidget(lbl_sc)
         # License
-        box.addWidget(QLabel(self.tr("License: <b>Edis</b> is licensed under "
-                                     "the terms of the <b>G</b>NU "
-                                     "<b>P</b>ublic <b>L</b>icense "
-                                     "version 3 or later.")))
+        #box.addWidget(QLabel(self.tr("License: <b>Edis</b> is licensed under "
+                                     #"the terms of the <b>G</b>NU "
+                                     #"<b>P</b>ublic <b>L</b>icense "
+                                     #"version 3 or later.")))
         # Thanks to
         group = QGroupBox(self.tr("Edis Team:"))
         group.setStyleSheet("QGroupBox { font-size: 16px; padding: 10px;"
@@ -68,7 +69,7 @@ class AcercaDe(QDialog):
         vbox.addWidget(QLabel("Gabriel Acosta <gabo>"))
         vbox.addWidget(QLabel("Martín Miranda <debianitram>"))
         vbox.addWidget(QLabel("Rodrigo Acosta <ekimdev>"))
-        box.addWidget(group)
+        #box.addWidget(group)
 
         box_boton = QHBoxLayout()
         box_boton.addSpacerItem(QSpacerItem(0, 10, QSizePolicy.Expanding))
